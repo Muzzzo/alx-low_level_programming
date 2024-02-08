594d81548ada922a15b35b09cf4e0b534cc26ab7
Muzzzo <musondachristopher@rocketmail.com>
2024-02-08 03:41:01 -0800

    search_algos.h

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -6,6 +6,7 @@
 #include <stdlib.h>
 
 /**
+ * struct listint_s - Singly linked list node structure
  *
  * @n: Integer
  * @index: Index of the node in the list
@@ -15,10 +16,11 @@
  */
 typedef struct listint_s
 {
-		int n;
-		size_t index;
-		struct listint_s *next;
-} listint_t;
+	int n;
+	size_t index;
+	struct listint_s *next;
+}
+listint_t;
 
 /**
  * struct skiplist_s - Singly linked list with an express lane
@@ -32,11 +34,12 @@ typedef struct listint_s
  */
 typedef struct skiplist_s
 {
-				int n;
-				size_t index;
-				struct skiplist_s *next;
-				struct skiplist_s *express;
-} skiplist_t;
+	int n;
+	size_t index;
+	struct skiplist_s *next;
+	struct skiplist_s *express;
+}
+skiplist_t;
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
@@ -46,6 +49,5 @@ int exponential_search(int *array, size_t size, int value);
 int advanced_binary(int *array, size_t size, int value);
 listint_t *jump_list(listint_t *list, size_t size, int value);
 skiplist_t *linear_skip(skiplist_t *list, int value);
-
 
 #endif /* SEARCH_ALGOS_H */
